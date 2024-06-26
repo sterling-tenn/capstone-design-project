619b72c9725d01895e5198280b95a05ce65b39ee
sterling-tenn <sterling8302@gmail.com>
2024-06-26 00:14:06 -0400

    bruh typo

--- simulator/helper.py
+++ simulator/helper.py
@@ -2,7 +2,7 @@ import math
 
 # return value at x, of a normal distribution pdf N(mu, sigma)
 def normal_distribution(mu, sigma, x):
-    exponent = -((x - mu) ** 2) / 2 * sigma ** 2
+    exponent = -((x - mu) ** 2) / (2 * sigma ** 2)
     return math.exp(exponent) / math.sqrt(2 * math.pi * sigma ** 2)
 
 def euclidian_distance(dest_x: float, dest_y: float, source_x: float, source_y: float, measurement_error: float):
