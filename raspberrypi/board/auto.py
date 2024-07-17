a3bc5d119b979f4049b33bbf03567ea169dd9c2e
sterling-tenn <sterling8302@gmail.com>
2024-07-17 13:35:21 -0400

    auto movement works, need to adjust params

--- raspberrypi/board/auto.py
+++ raspberrypi/board/auto.py
@@ -1,46 +1,67 @@
 # automatic predefined movement sequence
 import time
+from gpiozero import Servo
 
-def move_forward(time):
-    # TODO
-    
-    time.sleep(time)
-    stop(1)
-    return
+# Set GPIO for servos
+left_servo = Servo(27) # GPIO 27, physical pin 13
+right_servo = Servo(17) # GPIO 17, physical pin 11
 
-def move_backward(time):
-    # TODO
+FORWARD = 1
+BACKWARD = -1
+STOP_TIME = 0.5 # how long we should stop between movements
 
-    time.sleep(time)
-    stop(1)
-    return
+SERVO_SPEED = 0.5 # adjust so we can calculate time to move specified certain distance
 
-def move_left(time):
-    # TODO
+def move_forward(distance):
+    left_servo.value = FORWARD
+    right_servo.value = BACKWARD
 
-    time.sleep(time)
-    stop(1)
-    return
+    time.sleep(distance / SERVO_SPEED)
+    stop(STOP_TIME)
 
-def move_right(time):
-    # TODO
+def move_backward(distance):
+    left_servo.value = BACKWARD
+    right_servo.value = FORWARD
 
-    time.sleep(time)
-    stop(1)
-    return
+    time.sleep(distance / SERVO_SPEED)
+    stop(STOP_TIME)
 
-def stop(time):
-    # TODO
-    time.sleep(time)
-    return
+# counter clockwise
+def turn_left(distance):
+    left_servo.value = BACKWARD
+    right_servo.value = BACKWARD
+
+    time.sleep(distance / SERVO_SPEED)
+    stop(STOP_TIME)
+
+# clockwise
+def turn_right(distance):
+    left_servo.value = FORWARD
+    right_servo.value = FORWARD
+
+    time.sleep(distance / SERVO_SPEED)
+    stop(STOP_TIME)
+
+def stop(t):
+    left_servo.detach()
+    right_servo.detach()
+    time.sleep(t)
 
 def main():
-    while True:
-        move_forward(3)
-        move_backward(3)
-        move_left(3)
-        move_right(3)
-        stop(3)
+    # stop servos on program start
+    left_servo.detach()
+    right_servo.detach()
+
+    try:
+        input("Press Enter to start the automatic movement sequence")
+        while True:
+            move_forward(3)
+            move_backward(3)
+            turn_left(3)
+            turn_right(3)
+            stop(3)
+    except KeyboardInterrupt:
+        print("\nProgram interrupted by user. Exiting...")
 
 if __name__ == '__main__':
     main()
