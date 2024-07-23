57b02f9cacb53c6a348e871198f806e4d187ab52
sterling-tenn <sterling8302@gmail.com>
2024-07-23 15:34:02 -0400

    3 sensors babyyy

--- raspberrypi/board/auto.py
+++ raspberrypi/board/auto.py
@@ -1,12 +1,13 @@
-# automatic predefined movement sequence
 import time
 from gpiozero import Servo, DistanceSensor
 import threading
 import json
 from conf import *
 
-# Set GPIO for ultrasonic sensor
-sensor = DistanceSensor(trigger=TRIGGER_PIN, echo=ECHO_PIN)
+# Set GPIO for ultrasonic sensors
+sensor_centre = DistanceSensor(trigger=TRIGGER_PIN_CENTRE, echo=ECHO_PIN_CENTRE)
+sensor_left = DistanceSensor(trigger=TRIGGER_PIN_LEFT, echo=ECHO_PIN_LEFT)
+sensor_right = DistanceSensor(trigger=TRIGGER_PIN_RIGHT, echo=ECHO_PIN_RIGHT)
 
 # Set GPIO for servos
 left_servo = Servo(LEFT_SERVO_PIN)
@@ -14,7 +15,7 @@ right_servo = Servo(RIGHT_SERVO_PIN)
 
 FORWARD = 1
 BACKWARD = -1
-STOP_TIME = 0.5 # how long we should stop between movements
+# STOP_TIME = 0.5 # how long we should stop between movements
 
 SERVO_FACTOR = 7.5 # adjust so we can calculate time to move specified certain distance
 TURNING_FACTOR = 90 / 1.3 # adjust so we can calculate time to turn specified angle
@@ -25,7 +26,6 @@ def move_forward(distance):
     right_servo.value = BACKWARD
 
     time.sleep(distance * SERVO_FACTOR)
-    stop(STOP_TIME)
 
 # estimated distance in metres
 def move_backward(distance):
@@ -33,7 +33,6 @@ def move_backward(distance):
     right_servo.value = FORWARD
 
     time.sleep(distance * SERVO_FACTOR)
-    stop(STOP_TIME)
 
 # counter clockwise
 # estimated degrees to turn
@@ -42,7 +41,6 @@ def turn_left(degrees):
     right_servo.value = BACKWARD
 
     time.sleep(degrees / TURNING_FACTOR)
-    stop(STOP_TIME)
 
 # clockwise
 # estimated degrees to turn
@@ -51,25 +49,26 @@ def turn_right(degrees):
     right_servo.value = FORWARD
 
     time.sleep(degrees / TURNING_FACTOR)
-    stop(STOP_TIME)
 
-def stop(t):
+def stop():
     left_servo.detach()
     right_servo.detach()
-    time.sleep(t)
 
-def read_sensor(sensor, stop_event):
+def read_sensors(sensors, stop_event):
     while not stop_event.is_set():
-        distance = sensor.distance * 100  # convert to cm
-        print(f"Distance: {distance:.2f} cm")
-        time.sleep(0.5)
-        
+        centre, left, right = sensors
+        c = centre.distance * 100  # convert to cm
+        l = left.distance * 100
+        r = right.distance * 100
+        print(f"[Distances] Left: {l:.2f} cm | Centre: {c:.2f} cm | Right: {r:.2f} cm")
+        time.sleep(0.1)
+
 def move_robot():
     with open("path.json", "r") as file:
         data = json.load(file)
         directions = data["directions"]
 
-    # orientation and path to follow taken 
+    # orientation and path to follow taken
     # care of in the json direction file
     # here, we just move
     for direction in directions:
@@ -91,12 +90,15 @@ def main():
     try:
         input("Press Enter to start the automatic movement sequence")
 
-        # to constantly read sensor in a separate thread
+        # to constantly read sensors in a separate thread
         stop_event = threading.Event()
-        sensor_thread = threading.Thread(target=read_sensor, args=(sensor, stop_event))
+        sensors = [sensor_centre, sensor_left, sensor_right]
+        sensor_thread = threading.Thread(target=read_sensors, args=(sensors, stop_event))
         sensor_thread.start()
 
         move_robot()
+        # while True:
+        #     time.sleep(1)
 
     except KeyboardInterrupt:
         print("\nProgram interrupted by user. Exiting...")
@@ -105,8 +107,10 @@ def main():
         left_servo.detach()
         right_servo.detach()
         
-        # close sensor and sensor thread
-        sensor.close()
+        # close sensors and sensor thread
+        sensor_centre.close()
+        sensor_left.close()
+        sensor_right.close()
         stop_event.set()
         sensor_thread.join()
 
--- raspberrypi/board/conf.py
+++ raspberrypi/board/conf.py
@@ -5,5 +5,11 @@ LEFT_SERVO_PIN = 27 # GPIO 27, physical pin 13
 RIGHT_SERVO_PIN = 17 # GPIO 17, physical pin 11
 
 # Sensor pins
-TRIGGER_PIN = 23 # GPIO 23, physical pin 16
-ECHO_PIN = 24 # GPIO 24, physical pin 18
+TRIGGER_PIN_CENTRE = 23 # GPIO 23, physical pin 16
+ECHO_PIN_CENTRE = 24 # GPIO 24, physical pin 18
+
+TRIGGER_PIN_LEFT = 5 # GPIO 5, physical pin 29
+ECHO_PIN_LEFT = 6 # GPIO 6, physical pin 31
+
+TRIGGER_PIN_RIGHT = 13 # GPIO 13, physical pin 33
+ECHO_PIN_RIGHT = 19 # GPIO 19, physical pin 35
