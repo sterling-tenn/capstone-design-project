fce6dd75e68cffaea5acf98b939ead6276ff0f24
sterling-tenn <sterling8302@gmail.com>
2025-02-01 14:41:43 -0500

    Update copy_files.sh

--- raspberrypi/control-machine/copy_files.sh
+++ raspberrypi/control-machine/copy_files.sh
@@ -4,18 +4,19 @@
 # List of files to send
 # servo.py, ultrasonic.py, gyroscope.py are temp for testing
 LOCAL_FILES=(
-            "C:/Users/sterl/Desktop/capstone-design-project/raspberrypi/board/conf.py"
-            "C:/Users/sterl/Desktop/capstone-design-project/raspberrypi/board/auto.py"
-            "C:/Users/sterl/Desktop/capstone-design-project/raspberrypi/board/manual.py"
-            "C:/Users/sterl/Desktop/capstone-design-project/raspberrypi/board/path.json"
+            # "C:/Users/sterl/Desktop/capstone-design-project/raspberrypi/board/conf.py"
+            # "C:/Users/sterl/Desktop/capstone-design-project/raspberrypi/board/auto.py"
+            # "C:/Users/sterl/Desktop/capstone-design-project/raspberrypi/board/manual.py"
+            # "C:/Users/sterl/Desktop/capstone-design-project/raspberrypi/board/path.json"
+            "C:/Users/sterl/Desktop/capstone-design-project/raspberrypi/board/server.py"
 
-            "C:/Users/sterl/Desktop/capstone-design-project/raspberrypi/board/gyroscope.py"
+            # "C:/Users/sterl/Desktop/capstone-design-project/raspberrypi/board/gyroscope.py"
             # "C:/Users/sterl/Desktop/capstone-design-project/raspberrypi/board/servo.py"
             # "C:/Users/sterl/Desktop/capstone-design-project/raspberrypi/board/ultrasonic.py"
             )
 
 REMOTE_USER="raspberrypi"
-REMOTE_HOST="192.168.137.176"
+REMOTE_HOST="192.168.226.60"
 REMOTE_DIR="/home/raspberrypi/CargoBuddy" # Working directory to receive files
 
 for FILE in "${LOCAL_FILES[@]}"; do
