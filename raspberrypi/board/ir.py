814c94faddc2873a8ee9f04e78b7ae8cef99db7a
sterling-tenn <sterling8302@gmail.com>
2025-02-01 15:56:07 -0500

    Create ir.py

---
+++ raspberrypi/board/ir.py
@@ -0,0 +1,9 @@
+# ADJUST KNOB ON SENSOR TO ADJUST SENSITIVITY
+
+from gpiozero import LineSensor
+from signal import pause
+
+sensor = LineSensor(4)
+sensor.when_line = lambda: print('Line detected')
+sensor.when_no_line = lambda: print('No line detected')
+pause()
