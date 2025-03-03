274d941ca082fcddf7b7a0798c1e29e854d9b5b6
Anthony <canzona4@outlook.com>
2025-03-03 01:47:24 +0000

    Demo maps and path

--- raspberrypi/board/auto_mcl.py
+++ raspberrypi/board/auto_mcl.py
@@ -1,4 +1,4 @@
 from robot_controller import RobotController
 
 controller = RobotController()
-controller.run('auto_mcl', '/home/raspberrypi/capstone-design-project/raspberrypi/board/path_b.json', '/home/raspberrypi/capstone-design-project/raspberrypi/board/map_b.json')
+controller.run('auto_mcl', '/home/raspberrypi/capstone-design-project/raspberrypi/board/path_demo.json', '/home/raspberrypi/capstone-design-project/raspberrypi/board/map_demo.json')
